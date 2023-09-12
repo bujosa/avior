6a32041d254956e07c07a0e56fc015813469dc05
bujosa <davidbujosa@gmail.com>
2023-09-12 15:17:00 -0400

    feat: run my first serve in bun

--- index.ts
+++ index.ts
@@ -1,1 +1,8 @@
-console.log("Hello via Bun!");
+const server = Bun.serve({
+  port: 3000,
+  fetch(req) {
+    return new Response('Bun!');
+  },
+});
+
+console.log(`Listening on http://localhost:${server.port} ...`);
